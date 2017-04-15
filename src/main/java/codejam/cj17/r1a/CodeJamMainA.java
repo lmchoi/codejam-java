3a9c64578953fc6c50f4336bd4126ccafa1d56eb
Mandy Choi <hello@mandychoi.com.au>
2017-04-15 06:29:54 +0100

    2017 R1A Problem A solved

--- src/main/java/codejam/cj17/r1a/CodeJamMainA.java
+++ src/main/java/codejam/cj17/r1a/CodeJamMainA.java
@@ -18,101 +18,84 @@ public class CodeJamMainA {
             int numOfCases = in.nextInt();
             in.nextLine(); // skip to next line
             for (int cn = 1; cn <= numOfCases; cn++) {
-
                 int r = in.nextInt();
                 int c = in.nextInt();
+
                 char[][] cake = new char[r][c];
-                String row = in.next();
-                char[] rowAbove = row.toCharArray();
-
-                char pc = rowAbove[0];
-
-                for (int i = 1; i < c; i++) {
-                    char cp = rowAbove[i];
-                    if (pc == '?') {
-                        if (cp != pc) {
-                            rowAbove[i - 1] = cp;
-                        }
-                    } else {
-                        if (cp == '?') {
-                            rowAbove[i] = pc;
-                        }
-                    }
-
-                    pc = rowAbove[i];
-                }
-
-                cake[0] = rowAbove;
-
-                for (int i = 1; i < r; i++) {
-                    row = in.next();
+                for (int i = 0; i < r; i++) {
+                    String row = in.next();
                     cake[i] = row.toCharArray();
 
-                    char previousPiece = cake[i][0];
-
-                    for (int j = 1; j < c; j++) {
-                        char cPiece = cake[i][j];
-
-                        if (previousPiece == '?') {
-                            if (cPiece != previousPiece) {
-                                for (int k = j - 1; k > -1; k--) {
-                                    if (cake[i][k] == '?' &&
-                                            rowAbove[k] != cPiece) {
-                                        cake[i][k] = cPiece;
-                                    } else {
-                                        break;
-                                    }
-                                }
-                            }
-                        } else {
-                            if (cPiece == '?') {
-                                cPiece = previousPiece;
-                            }
-                        }
-
-                        cake[i][j] = cPiece;
-
-                        previousPiece = cPiece;
-                    }
-
+                    int qs = -1;
+                    char name = '?';
                     for (int j = 0; j < c; j++) {
-                        if (rowAbove[j] == '?') {
-                            for (int k = i - 1; k > -1; k--) {
-                                if (cake[k][j] == '?') {
-                                    cake[k][j] = cake[i][j];
-                                } else {
-                                    break;
+                        if (cake[i][j] == '?') {
+                            if (name == '?') {
+                                if (qs == -1) {
+                                    qs = j;
                                 }
+                            } else {
+                                cake[i][j] = name;
                             }
                         } else {
-                            if (cake[i][j] == '?') {
-                                cake[i][j] = cake[i - 1][j];
+                            if (qs != -1) {
+                                for (int k = qs; k < j; k++) {
+                                    cake[i][k] = cake[i][j];
+                                }
+                                qs = -1;
                             }
+
+                            name = cake[i][j];
                         }
                     }
+                }
 
-                    rowAbove = cake[i];
+
+                for (int j = 0; j < c; j++) {
+                    int qs = -1;
+                    char name = '?';
+                    for (int i = 0; i < r; i++) {
+                        if (cake[i][j] == '?') {
+                            if (name == '?') {
+                                if (qs == -1) {
+                                    qs = i;
+                                }
+                            } else {
+                                cake[i][j] = name;
+                            }
+                        } else {
+                            if (qs != -1) {
+                                for (int k = qs; k < i; k++) {
+                                    cake[k][j] = cake[i][j];
+                                }
+                                qs = -1;
+                            }
+
+                            name = cake[i][j];
+                        }
+                    }
                 }
 
                 // output
-                System.out.print("Case #" + cn + ":");
+                System.out.println("Case #" + cn + ":");
                 printMatrix(cake);
             }
+
         }
 
         public void printMatrix(char[][] board) {
             StringBuilder sb = new StringBuilder();
 
             for (int i = 0; i < board.length; i++) {
-                sb.append("\n");
-
-                for (int j = 0; j < board[i].length; j++) {
+                int c = board[i].length;
+                for (int j = 0; j < c; j++) {
                     sb.append(board[i][j]);
                 }
 
+                sb.append("\n");
             }
 
-            System.out.println(sb.toString());
+            System.out.print(sb.toString());
         }
     }
 }
